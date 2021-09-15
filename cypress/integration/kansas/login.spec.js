3997c095bc91ec3d805c844245ab73dceeb0a80b
echavezp <Edgar2020>
2021-09-14 21:20:01 -0600

    finished user test case

--- cypress/integration/kansas/login.spec.js
+++ cypress/integration/kansas/login.spec.js
@@ -16,6 +16,7 @@ describe('Validate Login', () => {
     cy.get('input:first').clear().type('useradminclient1')
     cy.get('input:last').clear().type('Aa123456')
     cy.get(':nth-child(4) > .md-button > .md-ripple > .md-button-content').click()
+    cy.screenshot()
     cy.contains('.alert > span', 'Fallo la Autenticación - Contacte a su administrador si en caso persiste el problema')
   })
 })
